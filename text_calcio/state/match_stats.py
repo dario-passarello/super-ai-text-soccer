05f56dc91d5f5ed3a42ae5221871a2694efd572a
Davide Merli <davide.merli@outlook.com>
2024-08-21 00:29:16 +0200

    More readable code

--- text_calcio/state/match_stats.py
+++ text_calcio/state/match_stats.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from collections import defaultdict
 from dataclasses import dataclass
 from typing import Literal, Optional
 
@@ -13,11 +12,11 @@ class MatchStats:
     team_1_stats: TeamStats
     team_2_stats: TeamStats
 
-    @staticmethod
-    def create_from_match(match: Match):
-        return MatchStats(
-            TeamStats.create_from_match(match, 0),
-            TeamStats.create_from_match(match, 1),
+    @classmethod
+    def create_from_match(cls, match: Match) -> MatchStats:
+        return cls(
+            TeamStats.create_from_match(match, team_id=0),
+            TeamStats.create_from_match(match, team_id=1),
         )
 
 
@@ -29,11 +28,11 @@ class TeamStats:
     score: int
     n_attempts: int
     goals: list[GoalStats]
-    ball_possesion_pct: float
+    ball_possession_percentage: float
     player_evaluation: dict[str, int]
 
     @staticmethod
-    def create_from_match(match: Match, team_id: Literal[0, 1]):
+    def create_from_match(match: Match, team_id: Literal[0, 1]) -> TeamStats:
         all_actions = match.get_all_actions_to_now()
         team = match.teams[team_id]
 
@@ -46,21 +45,16 @@ class TeamStats:
         score = match.get_current_score()[team_id]
 
         goals = [
-            goal
+            GoalStats.create_from_action(action)
             for action in all_actions
-            if action.team_atk_id == team_id
-            and (goal := GoalStats.create_from_action(action)) is not None
+            if action.team_atk_id == team_id and action.is_goal()
         ]
 
-        if len(all_actions) == 0:
-            ball_possesion_pct = 0
-        else:
-            ball_possesion_pct = n_attempts / len(all_actions) * 100
+        ball_possession_percentage = (
+            (n_attempts / len(all_actions) * 100) if all_actions else 0
+        )
 
-        all_evaluations = defaultdict(int)
-
-        for pl in team.players:
-            all_evaluations[pl] = 0
+        player_evaluation = {player: 0 for player in team.players}
 
         for action in all_actions:
             team_role = "atk" if action.team_atk_id == team_id else "def"
@@ -70,17 +64,17 @@ class TeamStats:
                 player_name = action.player_assigments[role]
 
                 if team_role in role:
-                    all_evaluations[player_name] += score
+                    player_evaluation[player_name] += score
 
         return TeamStats(
-            match.curr_phase,
-            match.curr_minute,
-            team,
-            score,
-            n_attempts,
-            goals,
-            ball_possesion_pct,
-            all_evaluations,
+            updated_at_phase=match.curr_phase,
+            updated_at_minute=match.curr_minute,
+            team=team,
+            score=score,
+            n_attempts=n_attempts,
+            goals=goals,
+            ball_possession_percentage=ball_possession_percentage,
+            player_evaluation=dict(player_evaluation),
         )
 
 
@@ -100,12 +94,16 @@ class GoalStats:
         assert action.goal_player is not None and action.type != "no_goal"
 
         scorer_player = action.map_role_to_name(action.goal_player)
-
-        if action.assist_player is None:
-            assist_player = None
-        else:
-            assist_player = action.map_role_to_name(action.assist_player)
+        assist_player = (
+            action.map_role_to_name(action.assist_player)
+            if action.assist_player
+            else None
+        )
 
         return GoalStats(
-            scorer_player, action.phase, action.minute, assist_player, action.type
+            author=scorer_player,
+            match_phase=action.phase,
+            minute=action.minute,
+            assist=assist_player,
+            goal_type=action.type,
         )
